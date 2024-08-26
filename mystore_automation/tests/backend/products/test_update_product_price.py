d964367e7c3f4fcfbd15cc56481b045dd3c0ac86
karajin <karajin1874@gmail.com>
2024-08-26 15:43:25 -0700

    modifiled test_update_product_price.py file

--- mystore_automation/tests/backend/products/test_update_product_price.py
+++ mystore_automation/tests/backend/products/test_update_product_price.py
@@ -9,21 +9,7 @@ import string
 @pytest.mark.ecombe30
 
 def test_update_regular_price_should_update_price():
-  prodcut_helper = ProductsAPIHelper()
-  product_dao = ProductsDAO()
-# find the prodcut list that's no onsale
-  payload = {
-    'on_sale': False,
-    'per_page' : 100
-  }
-
-  product_not_on_sale = prodcut_helper.call_get_product(payload)
-
-  if product_not_on_sale:
-    selected_product = random.choice(product_not_on_sale)
-    product_id = selected_product['id']
-  else:
-    
+    pass
 
 
 #if found products no one sale, pick a random prodcut
